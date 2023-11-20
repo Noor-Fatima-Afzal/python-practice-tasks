a71370599ea234a9096092bafa53da05c71735d6
Noor-Fatima-Afzal <noorfatimaafzalbutt@gmail.com>
2023-11-21 00:15:58 +0500

    task is added

---
+++ ArmstrongNumber.py
@@ -0,0 +1,14 @@
+num=int(input("enter a number"))
+num_str = str(num)
+num_digits = len(num_str)
+sum=0
+temp=num
+while temp > 0:
+    remainder=temp%10
+    power=remainder**num_digits
+    sum=sum+power
+    temp=temp//10
+if sum==num:
+    print("is is an armstrong number")
+else:
+    print("not")
---
+++ sumOfNaturalnumber.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number whose sum you want to calculate"))
+for i in range(1,num):
+    num=num+i
+    i=i+1
+print(num)
