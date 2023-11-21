532258de3489591280e7f7c8d14aba00ebf9ae34
Noor-Fatima-Afzal <noorfatimaafzalbutt@gmail.com>
2023-11-21 06:34:31 +0500

    armstrong number task aded

--- ArmstrongNumber.py
+++ ArmstrongNumber.py
@@ -1,14 +1,15 @@
-num=int(input("enter a number"))
-num_str = str(num)
-num_digits = len(num_str)
-sum=0
-temp=num
-while temp > 0:
-    remainder=temp%10
-    base=remainder**num_digits
-    sum=sum+base
-    temp=temp//10
-if sum==num:
-    print("is is an armstrong number")
-else:
-    print("not an armstrong number")
+lower=int(input("enter lower number"))
+uper=int(input("enter uper number"))
+for num in range(lower,uper+1):
+    numstr = str(num)
+    num_digits = len(numstr)
+    sum=0
+    temp=num
+    while temp > 0:
+        remainder=temp%10
+        base=remainder**num_digits
+        sum=sum+base
+        temp=temp//10
+    if sum==num:
+        print(sum,"is is an armstrong number")
+    
