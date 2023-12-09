2e47c806267477d238411909312155f41d8ebb0b
Noor-Fatima-Afzal <noorfatimaafzalbutt@gmail.com>
2023-12-09 15:01:14 +0500

    functions

---
+++ week#2/adressbookDict.py
@@ -0,0 +1,5 @@
+addressbook={"noor":{"city":"wzd","clg":"pgc","uni":"uet"},
+             "iqra":{"city":"wzd","clg":"pgc","uni":"vu"},
+             "amna":{"city":"wzd","clg":"pgc","uni":"pu"}}
+print("iqra's infor",addressbook["iqra"])
+print("iqra's uni",addressbook["iqra"]["uni"])
---
+++ week#2/functionWITHINfunction.py
@@ -0,0 +1,11 @@
+def print_twice(a):
+    print(a, a)
+
+def join(b, c):
+    result = b + c
+    return result 
+
+b = "noor"
+c = "fatima"
+joined_string = join(b, c)
+print_twice(joined_string)
