1de7232af10eb14e72be7eeb8f3d24c82fe7b26b
Noor Fatima <noorfatimaafzalbutt@gmail.com>
2023-12-19 13:58:30 +0500

    lab work

--- Lab 4 Tuples Dictionaries and Functions in Python/keyEXISTorNot.py
+++ Lab 4 Tuples Dictionaries and Functions in Python/keyEXISTorNot.py
@@ -1,10 +1,12 @@
 dict={
     '1':2,
-    '5':5,
+    '5':"noor",
     '3':1,
 }
 key_value=input("enter key")
 if key_value in dict.keys():
     print(True)
+    print("key=",key_value)
+    print("value=",dict[key_value])
 else:
     print(False)    
